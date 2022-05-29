1fa250e069082cfcaed793eb0c3d05253dae9fba
Ivo Uenk <mail@udirection.com>
2022-05-29 21:50:17 +0200

    a

--- PSScripts/Install-Choco-Apps.ps1
+++ PSScripts/Install-Choco-Apps.ps1
@@ -14,10 +14,12 @@ else {
 if ((!($regexist)) -or ($regexist.$RegCheck -lt $Version)) {
     try{
         Set-ExecutionPolicy Bypass -Scope Process -Force
-        iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
-	choco install adobereader -params '"/NoUpdates"' -y
-	choco install Firefox --params "/l:nl-NL /NoDesktopShortcut /NoMaintenanceService /RemoveDistributionDir" -y
+        Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    
+        choco install chocolateygui -y
+	    choco install adobereader -params '"/NoUpdates"' -y
+        choco install googlechrome -y
+	    choco install Firefox --params "/l:nl-NL /NoDesktopShortcut /NoMaintenanceService /RemoveDistributionDir" -y
 
         $location = "C:\Users\Public\Desktop\"
         if(Test-Path $location){
---
+++ PSScripts/Repair-AppStore.ps1
@@ -0,0 +1,1 @@
+Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
